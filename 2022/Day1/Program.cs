b6659b03d61073fd968edbf3f222e480d921b4c5
Stanisław Nieradko <stanislaw@nieradko.com>
2022-12-01 06:58:14 +0100

    meta: create repository and solve day 1

---
+++ 2022/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿var caloriesByElf = File.ReadAllText("input.txt")
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(inventory => inventory
+        .Split(Environment.NewLine)
+        .Where(value => !string.IsNullOrWhiteSpace(value))
+        .Select(int.Parse)
+        .Sum()
+    )
+    .ToArray();
+
+Console.WriteLine($"Puzzle 1: {caloriesByElf.Max()}");
+
+var sumOfTop3CaloriesCarriedByElf = caloriesByElf
+    .OrderBy(x => x)
+    .TakeLast(3)
+    .Sum();
+
+Console.WriteLine($"Puzzle 2: {sumOfTop3CaloriesCarriedByElf}");
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code
+
+Repository with my solution to [Advent of Code](https://adventofcode.com).
